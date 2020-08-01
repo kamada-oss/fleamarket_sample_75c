3f2d574e618a6715040e6a07f05f8c6a7758f23a
kamada-oss <kamada.engineer@gmail.com>
2020-08-01 14:08:39 +0900

    pull master

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
+  devise_for :users
   resources :signup, only: [:index, :create] do
     collection do
       get 'registration_user_information'
