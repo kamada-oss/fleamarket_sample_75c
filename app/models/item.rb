8b3e53ca4dd89ae708411647d89f40c83e1a63f6
urataku1217 <urataku.com@gmail.com>
2020-08-14 16:17:10 +0900

    edit code

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,6 +14,7 @@ class Item < ApplicationRecord
   }
   enum auction_status: {出品中:1,  売り切れ:2}
   has_one    :purchase
+  belongs_to :user
   belongs_to :category
   belongs_to :brand, optional: true
   has_many   :likes, dependent: :destroy
