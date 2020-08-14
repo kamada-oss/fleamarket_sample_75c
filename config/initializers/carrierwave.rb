d057bbc82691b083f5f67d6eb0f08a7198df4647
kamada-oss <kamada.engineer@gmail.com>
2020-08-14 22:09:13 +0900

    ヘッダーのリンクを修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,7 +15,7 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1' # 東京はap-northeast-1
     }
     config.fog_directory  = 'fleamarket-sample-75c' #S3のバケット名
-    config.asset_host = 'https://s3-your_region.amazonaws.com/fleamarket-sample-75c'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fleamarket-sample-75c'
   else
     config.storage :file # 開発環境:public/uploades下に保存
     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
