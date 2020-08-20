72463b7f23f8fcaa242f71f5927ab6116b4a78a1
midori-panisa <64828177+midori-panisa@users.noreply.github.com>
2020-08-20 15:18:31 +0900

    edit-code

--- app/assets/javascripts/edit_items.js
+++ app/assets/javascripts/edit_items.js
@@ -13,7 +13,6 @@ $(function(){
       var image_id = Number($('#image-box-1').attr('class'))
       var append_div_count = Number($('div[id=1]').length) 
       var noreset_id = image_id + append_div_count
-      
       fileReader.readAsDataURL(file);
      //画像が10枚になったら超えたらドロップボックスを削除する
       if (num == 10){
@@ -111,89 +110,7 @@ $(function(){
     $('#image-box__container').show()
     $('#image-box__container').attr('class', `item-num-${num}`)
   });
-
-  var dropArea = $(".item-num-0");
-  dropArea.on("dragenter", function(e){
-    e.stopPropagation();
-    e.preventDefault();
-  });
-  //ドラッグした要素がドロップターゲットの上にある時にイベントが発火
-  dropArea.on("dragover", function(e){
-    e.stopPropagation();
-    e.preventDefault();
-    //ドロップエリアに影がつく
-    $(this).css({'border': '1px solid rgb(204, 204, 204)','box-shadow': '0px 0px 4px'});
-  });
-  dropArea.on("dragleave", function(e){
-    e.preventDefault();
-    e.preventDefault();
-   //ドロップエリアの影が消える
-    $(this).children.css({'border': '1px dashed rgb(204, 204, 204)','box-shadow': '0px 0px 0px'})
-  },false);
-  //loadイベント発生時に発火するイベント
-  window.onload = function(e){
-    e.preventDefault();
-    //ドラッグした要素がドロップターゲットから離れた時に発火するイベント
-    dropArea[0].addEventListener("dragleave", function(e){
-      e.stopPropagation()
-      e.preventDefault();
-    //ドロップエリアの影が消える
-      $(this).css({'border': '1px dashed rgb(204, 204, 204)','box-shadow': '0px 0px 0px'})
-    },false);
-    //ドラッグした要素をドロップした時に発火するイベント
-    dropArea[0].addEventListener("drop", function(e) {
-      e.stopPropagation()
-      e.preventDefault();
-      $(this).css({'border': '1px dashed rgb(204, 204, 204)','box-shadow': '0px 0px 0px'}),false;
-      var files = e.dataTransfer.files;
-      $("#img-file")[0].files = files;
-      //ドラッグアンドドロップで取得したデータについて、プレビューを表示
-      $.each(files, function(i,file){
-        //アップロードされた画像を元に新しくfilereaderオブジェクトを生成
-        var fileReader = new FileReader();
-        //dataTransferオブジェクトに値を追加
-        dataBox.items.add(file)
-        //lengthで要素の数を取得
-        var num = $('.item-image').length + i + 1
-        var aaa = $('.item-image').length + i
-        //指定されたファイルを読み込む
-        fileReader.readAsDataURL(file);
-        // 10枚プレビューを出したらドロップボックスが消える
-        if (num==10){
-          $('#image-box__container').css('display', 'none')
-        }
-        //image fileがロードされた時に発火するイベント
-        fileReader.onload = function() {
-          //変数srcにresultで取得したfileの内容を代入
-          var src = fileReader.result
-          var html= `<div class='item-image' data-image="${file.name}" data-index="${aaa}">
-                      <div class=' item-image__content'>
-                        <div class='item-image__content--icon'>
-                          <img src=${src} width="170" height="170" >
-                        </div>
-                      </div>
-                      <div class='item-image__ation'>
-                        <div class='item-image__operation--delete'>削除</div>
-                      </div>
-                    </div>`
-          const buildFileField = (num)=> {
-            const html = `<div  class="js-file_group" data-index="${num}">
-                            <input class="js-file-edit" type="file"
-                            name="item[item_images_attributes][${num}][item_image]"
-                            id="img-file" data-index="${num}">
-                          </div>`;
-            return html;
-          }
-        $('.js-file-edit').removeAttr('id');
-        //image-box__containerの前にhtmlオブジェクトを追加
-        $('.img-label').before(html);
-        $('#append-js-edit').append(buildFileField(num));
-        };
-        //image-box__containerにitem-num-(変数)という名前のクラスを追加する
-        $('#image-box__container').attr('class', `item-num-${num}`)
-      })
-    })
-  }
+  
     // windowにdropした時の挙動をキャンセル
     $(document).on("dragenter", function(e){
       e.stopPropagation();
