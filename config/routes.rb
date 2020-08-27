0587895a944baffe21cb74ba7537a8ecc29d1ade
urataku1217 <urataku.com@gmail.com>
2020-08-27 18:34:00 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'items/purchase'
   root 'items#index'
   resources :items do
+    resources :comments, only: :create
     collection do
       get 'search_child', defaults: { format: 'json' }
       get 'search_grandchild', defaults: { format: 'json' }
