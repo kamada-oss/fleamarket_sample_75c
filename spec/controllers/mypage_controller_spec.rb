c833852af8049e343017714090d72674cde3631d
kamada-oss <kamada.engineer@gmail.com>
2020-08-29 11:52:16 +0900

    マイページコントローラーのテストを実装

---
+++ spec/controllers/mypage_controller_spec.rb
@@ -0,0 +1,23 @@
+# require 'rails_helper'
+
+# describe MypageController do
+#   let(:user) { create(:user) }
+
+#   describe 'GET #good_index' do
+#     # context 'ログインしている場合' do
+#     #   before do
+#     #     login user
+#     #     get :item_exhibiting
+#     #   end
+#     # end
+#     context 'ログインしていない場合' do
+#       before do
+#         get :good_index
+#       end
+#       it 'トップページにリダイレクトされること' do
+#         expect(response).to redirect_to(root_path)
+#       end
+#     end
+#   end
+
+# end
